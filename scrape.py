e5b74d80c030b17a599825a62b3b8b03243ab9fa
ogimgio <34304398+ogimgio@users.noreply.github.com>
2022-09-17 23:39:15 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -12,6 +12,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
 import requests # 2
 import json # 3
 
