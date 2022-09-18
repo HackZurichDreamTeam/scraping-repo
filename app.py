23b3113e27456b8dd11fffc05d0d83996b693168
ogimgio <34304398+ogimgio@users.noreply.github.com>
2022-09-18 02:15:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
-from flask import Flask
+from flask import Flask,jsonify
 import json
 import pandas as pd
+import urllib.request, json 
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World."
+    return "Nothing here. "
+
 @app.route('/scrapeweather', methods=['GET'])
 def scrape_weather():
     #scrape weather call
     weather_data_path ='https://raw.githubusercontent.com/HackZurichDreamTeam/scraping-repo/main/scraped_weather.csv'
     got_df = pd.read_csv(weather_data_path)
     result = got_df.to_json(orient='records')
-    parsed = json.loads(result)
-    resu = json.dumps(parsed)
-    return resu
+    parsed = jsonify(result)
+    return parsed
 
 @app.route('/scrapepirates', methods=['GET'])
 def scrape_pirates():
-    #scrape weather call
+    #scrape pirates call
     weather_data_path ='https://raw.githubusercontent.com/HackZurichDreamTeam/scraping-repo/main/scraped_pirates.csv'
     got_df = pd.read_csv(weather_data_path)
     result = got_df.to_json(orient='records')
-    parsed = json.loads(result)
-    resu = json.dumps(parsed)
-    return resu
+    parsed = jsonify(result)
+    return parsed
+
+
+@app.route('/warnings', methods=['GET'])
+def get_warnings():
+    #get warnings
+    warnings_data_path = 'https://raw.githubusercontent.com/HackZurichDreamTeam/data_science/main/data_for_backend/warnings.json'
+    with urllib.request.urlopen(warnings_data_path) as url:
+        parsed = jsonify(json.load(url))
+
+    return parsed
+
+
+@app.route('/shipment', methods=['GET'])
+def get_shipment():
+    #get shipment info
+    shipment_data_path = 'https://raw.githubusercontent.com/HackZurichDreamTeam/data_science/main/data_for_backend/shipments.json'
+    with urllib.request.urlopen(shipment_data_path) as url:
+        parsed = jsonify(json.load(url))
+
+    return parsed
+
 
 if __name__ == "__main__":
     app.run()
